9a5b6eb6b3d6f60ed8028e2237f2d3e5427b29f7
jasontsang <jasontsang.icloud@me.com>
2017-11-21 10:56:16 +0800

    scroll stackview

--- TaoKe/ui/controller/NewerGuideController.swift
+++ TaoKe/ui/controller/NewerGuideController.swift
@@ -10,9 +10,9 @@ import UIKit
 import FontAwesomeKit
 
 class NewerGuideController: UIViewController, UIScrollViewDelegate {
-
-    @IBOutlet weak var newerGuideList: UICollectionView!
-    @IBOutlet weak var newerGuideListFlowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var scrollview: UIScrollView!
+    
+    @IBOutlet weak var newerGuideList: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +22,17 @@ class NewerGuideController: UIViewController, UIScrollViewDelegate {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: FAKFontAwesome.chevronLeftIcon(withSize: 15).image(with: CGSize(width: 15, height: 15)), style: .plain, target: self, action: #selector(back))
         navigationItem.title = "新手引导"
         
+        scrollview.translatesAutoresizingMaskIntoConstraints = false
+        newerGuideList.translatesAutoresizingMaskIntoConstraints = false
         
+        for _ in 1 ..< 20 {
+            let imageView = UIImageView(image: #imageLiteral(resourceName: "splash"))
+            newerGuideList.addArrangedSubview(imageView)
+            newerGuideList.addConstraint(NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: 100))
+            if let constraint = (newerGuideList.constraints.filter{$0.firstAttribute == .height}.first) {
+                constraint.constant += (100 + newerGuideList.spacing)
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,5 +43,4 @@ class NewerGuideController: UIViewController, UIScrollViewDelegate {
     @objc private func back() {
         navigationController?.popViewController(animated: true)
     }
-    
 }

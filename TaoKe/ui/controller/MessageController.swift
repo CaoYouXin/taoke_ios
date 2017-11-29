1f7d2861cacf7496082c930d136ef8ff013efb18
jasontsang <jasontsang.icloud@me.com>
2017-11-29 16:53:02 +0800

    message list

--- TaoKe/ui/controller/MessageController.swift
+++ TaoKe/ui/controller/MessageController.swift
@@ -11,9 +11,8 @@ class MessageController: UIViewController {
     private let disposeBag = DisposeBag()
     
     private var messageListHelper: MVCHelper<MessageView>?
-    private var cache: [Int:CGFloat] = [:]
     
-    private var messages: [String?]?
+    private var cache: [Int:CGFloat] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,26 +26,11 @@ class MessageController: UIViewController {
     }
 
     private func initList() {
-        messageList.mj_header = MJRefreshNormalHeader(refreshingBlock: {
-            self.messageListHelper?.refresh()
-            let delayTime = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-            DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                self.messageList.mj_header.endRefreshing()
-            }
-        })
-
-        messageList.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
-            self.messageListHelper?.loadMore()
-            let delayTime = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-            DispatchQueue.main.asyncAfter(deadline: delayTime) {
-                self.messageList.mj_footer.endRefreshing()
-            }
-        })
-        
         messageList.register(UINib(nibName: "MessageCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
         
         let messageListLayout = ELWaterFlowLayout()
         messageList.collectionViewLayout = messageListLayout
+        messageListLayout.prepare()
         
         messageListLayout.delegate = self
         messageListLayout.lineCount = 1
@@ -54,13 +38,24 @@ class MessageController: UIViewController {
         messageListLayout.hItemSpace = 10
         messageListLayout.edge = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
+        RxBus.shared.asObservable(event: Events.WaterFallLayout.self)
+            .throttle(RxTimeInterval(1), latest: true, scheduler: ConcurrentDispatchQueueScheduler(qos: .background))
+            .rxSchedulerHelper()
+            .subscribe { event in
+                messageListLayout.lineCount = 1
+            }.disposed(by: disposeBag)
+        
         let messageFactory: (UICollectionView, Int, MessageView) -> UICollectionViewCell = { (collectionView, row, element) in
             let indexPath = IndexPath(row: row, section: 0)
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MessageCell
             
+            cell.content.text = nil
+            
             cell.title.text = element.title
             cell.time.text = element.dateStr
             cell.content.text = element.content
+            
+            RxBus.shared.post(event: Events.WaterFallLayout())
             
             cell.layer.borderWidth = 1
             cell.layer.borderColor = UIColor("#ffd500").cgColor
@@ -72,15 +67,24 @@ class MessageController: UIViewController {
         messageListHelper = MVCHelper<MessageView>(messageList)
         messageListHelper?.set(cellFactory: messageFactory)
         messageListHelper?.set(dataSource: MessageDataSource(self))
-        messageListHelper?.set(dataHook: {messageViews in
-            self.messages = []
-            for messageView in messageViews {
-                self.messages?.append(messageView.content)
-            }
-            return messageViews
-        })
         
         messageListHelper?.refresh()
+        
+        messageList.mj_header = MJRefreshNormalHeader(refreshingBlock: {
+            self.messageListHelper?.refresh()
+            let delayTime = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: delayTime) {
+                self.messageList.mj_header.endRefreshing()
+            }
+        })
+        
+        messageList.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
+            self.messageListHelper?.loadMore()
+            let delayTime = DispatchTime.now() + Double(Int64(2 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: delayTime) {
+                self.messageList.mj_footer.endRefreshing()
+            }
+        })
     }
     
     private func initBadge() {
@@ -110,12 +114,16 @@ class MessageController: UIViewController {
 extension MessageController: ELWaterFlowLayoutDelegate  {
     func el_flowLayout(_ flowLayout: ELWaterFlowLayout, heightForRowAt index: Int) -> CGFloat {
         var cellHeight = CGFloat(60)
-        if let message = self.messages?[index] {
-            let maxCountPerLine = 40
-            let minHeightPerLine = CGFloat(20)
-            let padding = CGFloat(15)
-            let lines = (message.utf16.count / maxCountPerLine) + (message.utf16.count % maxCountPerLine > 0 ? 1 : 0)
-            cellHeight += (minHeightPerLine * CGFloat(lines) + padding)
+        
+        if let height = self.cache[index] {
+            cellHeight += (height + 15)
+        }else if self.messageList.numberOfItems(inSection: 0) > index {
+            if let cell = self.messageList.cellForItem(at: IndexPath(row: index, section: 0)) as? MessageCell {
+                cell.content.sizeToFit()
+                let height = cell.content.frame.size.height
+                self.cache[index] = height
+                cellHeight += (height + 15)
+            }
         }
         return cellHeight
     }
